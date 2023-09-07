22112e04a1dcca3b00953c654b6b085ba3999e0c
Tong Zhu <tzhu1997@outlook.com>
2023-09-07 15:22:30 +0800

    update ref links

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -8,7 +8,7 @@ import LastUpdate from './LastUpdate.vue';
         <p>
           Links:
           <a href="https://chen-wenliang.github.io/" target="_blank">Wenliang Chen</a>
-          | <a href="https://jacob-zhou.github.io/" target="_blank">Houquan Zhou</a>
+          | <a href="https://cv.hqzhou.com/" target="_blank">Houquan Zhou</a>
           | <a href="https://yzhangcs.github.io/" target="_blank">Yu Zhang</a>
         </p>
         <p>
